d3bad62fb7d7929dfb30fcfb7ea92d49e0a1d555
Patrick Kelly <patdkelly5@gmail.com>
2025-01-27 13:37:14 +1100

    Finished initial HTML structure and layout

--- PK-ToDo-React/src/ToDoList.jsx
+++ PK-ToDo-React/src/ToDoList.jsx
@@ -1,7 +1,18 @@
+// With my callback functions, whenever I pass in a function and add a set of parentheses right away,
+// like how I pass in my current index value to my deleteTask function,
+// It will CALL THE FUNCTION RIGHT AWAY, instead of waiting for the button to be clicked
+// I can get around this by using an arrow function, which will create a new function that will call my function ONLY WHEN the button is clicked
+//
+//
+
 import React, { useState } from "react";
 
 function ToDoList() {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([
+    "Eat breakfast",
+    "Take a shower",
+    "Walk the dog",
+  ]);
   const [newTask, setNewTask] = useState("");
 
   function handleInputChange(event) {
@@ -27,7 +38,27 @@ function ToDoList() {
           value={newTask}
           onChange={handleInputChange}
         />
+        <button className="add-button" onClick={addTask}>
+          Add
+        </button>
       </div>
+
+      <ol>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <span className="text">{task}</span>
+            <button className="delete-button" onClick={() => deleteTask(index)}>
+              Delete
+            </button>
+            <button className="move-button" onClick={() => moveTaskUp(index)}>
+              🔼
+            </button>
+            <button className="move-button" onClick={() => moveTaskDown(index)}>
+              🔽
+            </button>
+          </li>
+        ))}
+      </ol>
     </div>
   );
 }

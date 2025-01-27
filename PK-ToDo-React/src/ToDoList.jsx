e044e04d38cae1418055fb989b374b9714291987
Patrick Kelly <patdkelly5@gmail.com>
2025-01-27 13:29:17 +1100

    Added starter funtions, added input box functionality

--- PK-ToDo-React/src/ToDoList.jsx
+++ PK-ToDo-React/src/ToDoList.jsx
@@ -1,7 +1,35 @@
 import React, { useState } from "react";
 
 function ToDoList() {
-  return <></>;
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  function handleInputChange(event) {
+    setNewTask(event.target.value);
+  }
+
+  function addTask() {}
+
+  function deleteTask(index) {}
+
+  function moveTaskUp(index) {}
+
+  function moveTaskDown(index) {}
+
+  return (
+    <div className="to-do-list">
+      <h1>To Do List</h1>
+
+      <div>
+        <input
+          type="text"
+          placeholder="Please enter a task.."
+          value={newTask}
+          onChange={handleInputChange}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default ToDoList;
